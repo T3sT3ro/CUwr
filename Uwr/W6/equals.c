ee177b3b2b336f2327b4eea795134d8a8b1b59c3
Tooster <max@polarczyk.pl>
2017-12-04 21:38:54 +0100

    Add examples to W6/equals.c

--- Uwr/W6/equals.c
+++ Uwr/W6/equals.c
@@ -6,33 +6,64 @@
 #include <string.h>
 #include <stdio.h>
 
-int l1(int x) {
-    return 2 * x + 12;
-}
-
-int l2(int x) {
-    return 6 * x;
-}
 
 /// zwraca tablice o n+1 elementach
 /// t[0] oznacza ilosc argumentow
 /// t[1..n] argumenty w ktorych funkcje sa rowne
 int *equals(int (*f1)(int), int (*f2)(int), int a, int b) {
-    
-    int *args = calloc(b-a+2, sizeof(int)); //elements, args[0] holds iterator and size
-    memset(args, 0, (b-a+2)*sizeof(int)); // fill 0
+
+    int *args = calloc(b - a + 2, sizeof(int)); //elements, args[0] holds iterator and size
+    memset(args, 0, (b - a + 2) * sizeof(int)); // fill 0
     for (int i = a; i <= b; ++i)
-        if(f1(i) == f2(i))
+        if (f1(i) == f2(i))
             args[++args[0]] = i;
 
     return args;
 }
 
+// y = 2x+12
+int l1(int x) {
+    return 2 * x + 12;
+}
+
+// y = 6x
+int l2(int x) {
+    return 6 * x;
+}
+
+// y = 2x
+int l3(int x) {
+    return 2 * x;
+}
+
+// y = 4x + 1
+int l4(int x) {
+    return 4 * x + 1;
+}
+
+// y = x^2
+int p1(int x) {
+    return x * x;
+}
+
+// y = x^3 + 1
+int w1(int x) {
+    return x * x * x + 1;
+}
+
+void printArguments(int (*f1)(int), int (*f2)(int), int a, int b) {
+    int *args = equals(f1, f2, a, b);
+    for (int i = 0; i < args[0]; ++i)
+        printf("%d ", args[i + 1]);
+    printf("\n");
+}
+
 int main() {
 
-    int *args = equals(l1, l2, -10, 10);
-    for (int i = 0; i < args[0]; ++i)
-        printf("%d ", args[i]);
+    printArguments(l1, l2, -10, 10);
+    printArguments(l1, l1, -10, 10);
+    printArguments(l3, p1, -100, 100);
+    printArguments(l4, w1, -100, 100);
 
     return 0;
 }

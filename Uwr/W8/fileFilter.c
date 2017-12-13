a156754da940ed2bfe98354a0836b8058a06fff0
Tooster <max@polarczyk.pl>
2017-12-14 00:33:29 +0100

    Add fileFilter.c with fixed tests files
    Tests had bad file endings and it caused segmentation vaults on Windows and Linux.
    Fixed by rewriting the last line in files.

--- Uwr/W8/fileFilter.c
+++ Uwr/W8/fileFilter.c
@@ -9,7 +9,8 @@
 #include <ctype.h>
 
 #define DELIMETER "\n\t\r "
-#define SIZE 1024
+#define SIZE 6688
+int dbg = 0;
 
 int main(int argv, char *argc[]) {
     // otwieranie plików
@@ -22,35 +23,44 @@ int main(int argv, char *argc[]) {
         printf("Nie udalo sie otworzyc pliku '%s'", argc[1]);
         return EXIT_FAILURE;
     }
-    char *newFileName;
+    
+    char newFileName[60] = "";
     strcat(newFileName, argc[1]);
     strcat(newFileName, "_filtered");
     FILE *outputFile = fopen(newFileName, "w");
-
+   
+    if (outputFile == NULL) {
+        printf("Nie udalo sie utworzyc pliku wyjscia '%s'", newFileName);
+        return EXIT_FAILURE;
+    }
     // czytanie plików
 
-    char *word1;
-    char *word2;
+  
+    char *word1 = malloc(sizeof(char) * SIZE);
+    char *word2 = malloc(sizeof(char) * SIZE);
     char *line = malloc(sizeof(char) * SIZE);
     int currentSize = SIZE;
-
-    while (line != NULL) {
-        fgets(line, SIZE, inputFile);
-        while (line[strlen(line)-1] != '\n') {
+ 
+    while (fgets(line, SIZE, inputFile) != NULL) {	
+	while (strlen(line) > 0 && line[strlen(line) - 1] != '\n') {
             currentSize *= 2;
             line = realloc(line, sizeof(char) * currentSize);
+            word1 = realloc(word1, sizeof(char) * currentSize);
+            word2 = realloc(word2, sizeof(char) * currentSize);
+            fgets(line + strlen(line), currentSize / 2, inputFile);
         }
         word2 = strtok(line, DELIMETER);
         while (word2 != NULL) {
-            word2 = strtok(NULL, DELIMETER);
             if (strcmp(word1, word2) != 0) {
-                fprintf(outputFile, word2);
-                fprintf(outputFile, "%d", strlen(word2));
+                fprintf(outputFile, "%s%lu ", word2, strlen(word2));
                 strcpy(word1, word2);
             }
+            word2 = strtok(NULL, DELIMETER);
         }
+        fprintf(outputFile, "\n");
     }
-
+   
     fclose(outputFile);
     fclose(inputFile);
+    return EXIT_SUCCESS;
 }
